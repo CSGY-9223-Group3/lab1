655a79eee0e131ae5dd76026b3889888ecc9c614
zahradtj <trey.j.zahradka@gmail.com>
2024-09-17 12:18:03 -0500

    add read/write/update/delete note routes

--- pastebin.py
+++ pastebin.py
@@ -6,3 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
+
+
+@app.route('/note/<id>', methods=["GET"])
+def read_note(id):
+    return 'read {}'.format(id)
+
+
+@app.route('/note/<id>', methods=["PUT"])
+def create_note(id):
+    return 'put {}'.format(id)
+
+
+@app.route('/note/<id>', methods=["POST"])
+def update_note(id):
+    return 'post {}'.format(id)
+
+
+@app.route('/note/<id>', methods=["DELETE"])
+def delete_note(id):
+    return 'delete {}'.format(id)

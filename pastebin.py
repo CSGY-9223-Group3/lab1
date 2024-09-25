c822207303ee3155873af5c3ba33359793ddf064
Ethan S. <es5888@nyu.edu>
2024-09-25 16:56:05 -0400

    Generate a random SECRET_KEY if not set in environment
    - Added logic to check for the SECRET_KEY in environment variables.
    - If SECRET_KEY is not found, a random one is generated using .
    - Display a message prompting users to set the key as an environment variable for production.

--- pastebin.py
+++ pastebin.py
@@ -3,13 +3,16 @@ import json
 import logging
 from functools import wraps
 from logging.config import dictConfig
+import os
+
 
 import jwt
 from flask import Flask, Response, request
 
-# In a production environment, this should be stored securely (e.g., as an environment variable)
-SECRET_KEY = 'cf4d68b60166f3e77fa11213797e2ee9ace4e813eead3fdbc522079ecee9beb2'
-
+try:
+    SECRET_KEY = os.environ['SECRET_KEY']
+except KeyError:
+    raise ValueError("No SECRET_KEY set for Flask application. This is required.")
 
 # Define the token_required decorator
 def token_required(f):

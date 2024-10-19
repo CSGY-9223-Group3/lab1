d4224df6e86529a8af463eca6a14e6af1f5cab7a
zahradtj <trey.j.zahradka@gmail.com>
2024-10-19 14:52:38 -0500

    'refactor and improve' token decorator

--- pastebin.py
+++ pastebin.py
@@ -73,26 +73,31 @@ users = {}
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
+        errror = None
         token = None
+        current_user = "none"
         if "Authorization" in request.headers:
             auth_header = request.headers["Authorization"]
             try:
                 token = auth_header.split(" ")[1]
             except IndexError:
-                return Response("Invalid token format", 401)
+                error = Response("Invalid token format", 401)
 
         if not token:
-            return Response("Token is missing", 401)
+            error = Response("Token is missing", 401)
 
         try:
             data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
             current_user = data["sub"]
         except jwt.ExpiredSignatureError:
-            return Response("Token has expired", 401)
+            error = Response("Token has expired", 401)
         except jwt.InvalidTokenError:
-            return Response("Invalid token", 401)
+            error = Response("Invalid token", 401)
 
-        return f(current_user, *args, **kwargs)
+        if errror:
+            return errror
+        else:
+            return f(current_user, *args, **kwargs)
 
     return decorated
 
